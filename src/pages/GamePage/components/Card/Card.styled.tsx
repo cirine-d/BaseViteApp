f70dfbcc9d7d0c79fb8ebb21e422d5aa4b7c3483
cirine-d <cirine.dumazet@gmail.com>
2023-06-23 16:21:43 +0100

    Changed card styles

--- src/pages/GamePage/components/Card/Card.styled.tsx
+++ src/pages/GamePage/components/Card/Card.styled.tsx
@@ -1,16 +1,21 @@
 import styled from '@emotion/styled'
 
-export const FlippedCard = styled.div`
+const Card = styled.div`
+  border-radius: 12px;
+  box-shadow: 5px 5px #00000047;
+`
+
+export const FlippedCard = styled(Card)`
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: xxx-large;
   margin: 5px;
-  background-color: white;
+  background-color: #ffffffc4;
 `
 
-export const UnFlippedCard = styled.div`
-  background-color: #fffdcb;
+export const UnFlippedCard = styled(Card)`
+  background-color: #fffdcbde;
   color: transparent;
   margin: 5px;
 `
